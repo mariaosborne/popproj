5f53e7d6d4d0ab7a9b77ac63460296c25b9dd207
mariaosborne <maria.d.osborne@gmail.com>
2022-12-12 12:43:56 -0800

    add data sets

---
+++ R/norway_asfr.R
@@ -0,0 +1,21 @@
+#' norway_asfr
+#'
+#' A data set containing age specific fertility rates
+#' from Norway. Data downloaded from HFD July 2022.
+#'
+#
+#' @format A data frame
+#' \describe{
+#' \item{Year}{year}
+#' \item{Age}{age group}
+#' \item{ASFR}{age specific fertility rate}
+#' }
+#'
+#'
+#' @source
+#' Human Fertility Database. Max Planck Institute for Demographic Research (Germany)
+#' and Vienna Institute of Demography (Austria).
+#' Available at www.humanfertility.org (data downloaded on July 28th, 2022).
+#'
+#'
+"norway_asfr"
---
+++ R/norway_female_lifetable.R
@@ -0,0 +1,16 @@
+#' Norway Female Lifetable
+#'
+#' From HMD July 2022.
+#'
+#
+#' @format data frame
+#'
+#'
+#' @source
+#' HMD. Human Mortality Database. Max Planck Institute for Demographic Research (Germany),
+#' University of California, Berkeley (USA), and French Institute for
+#' Demographic Studies (France). Available at www.mortality.org
+#' (data downloaded on July 28th, 2022).
+#'
+#'
+"norway_female_lifetable"
